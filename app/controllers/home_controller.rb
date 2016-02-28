02c1f2c11fe085cbfebdf7c77d4ea882e6d721b6
vit2 <shiegin@gmail.com>
2016-02-28 21:34:21 +0000

    added user profile (UserController with show action)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
     def index
         unless user_signed_in?
             redirect_to :controller => 'devise/sessions', :action => 'new'
+        else
+            redirect_to :controller => 'users', :action => 'show', :id => current_user
         end
     end
 end
